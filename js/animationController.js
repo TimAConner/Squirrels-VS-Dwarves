83d2a141f55b22d263efc589ac86c7f7f973865d
Tim A. Conner <timaconner1@gmail.com>
2018-01-30 11:43:42 -0600

    Add AddAnimations function.

--- js/animationController.js
+++ js/animationController.js
@@ -2,9 +2,6 @@
 const g = require("./game");
 const img = require("./imageController");
 
-// Current time in timestamp updated on each selectNextFrame & shouldIncrementFrame
-let currentTime = 0;
-
 /*
 Animation Example:
 {
@@ -16,6 +13,39 @@ Animation Example:
     h: 21
 }
 */
+
+// Current time in miliseconds updated on each shouldIncrementFrame()
+let currentTime = 0;
+
+// Array of total animations
+let animations = [];
+
+const addAnimation = (name, animation) => {
+    let {frame, curFrame, lastFrame, interval, w, h} = animation;
+    let animationObject = {
+        name,
+        frame,
+        curFrame,
+        lastFrame, 
+        interval,
+        w,
+        h
+    };
+    animations.push(animationObject);
+};
+
+const findAnimation = (name) => {
+    return animations.find(anim => anim.name === name);
+};
+
+addAnimation('dwarfAnimation', {
+    frame: [1, 2],
+    curFrame: 0,
+    lastFrame: 0,
+    interval: 250,
+    w: 21,
+    h: 21
+});
 
 // Calculates location in the spritesheet of the current frame.
 const calcFrame = (animation) => {
@@ -37,12 +67,18 @@ const selectNextFrame = (animation) => {
 };
 
 // Draws player animation on the canvas then calls updateAnimation to get a new frame.
-const drawPlayerAnimation = (imgName, animation, position) => {
-
-    // Animations run along x axis, so the animations must be in a horizontal strip.
+// Animations run along x axis, so the animations must be in a horizontal strip.
+const drawPlayerAnimation = (imgName, animationName, position) => {
+    let animation = findAnimation(animationName);
     g.ctx.drawImage(img(imgName), calcFrame(animation), 0, animation.w, animation.h, position.x, position.y, g.playerSize, g.playerSize); 
-
     if(shouldIncrementFrame(animation)) selectNextFrame(animation);
 };
+
+/*
+ Export just drawPlayerAniation so this can be called refrenced by
+    const drawPlayerAnimation = require("./animationController");
+ and called by
+    drawPlayerAnimation();
+ */
 
 module.exports = drawPlayerAnimation;
