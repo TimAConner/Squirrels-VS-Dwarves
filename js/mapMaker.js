438606decdbb4be2aef60442adcc9c8642c2278b
Tim A. Conner <timaconner1@gmail.com>
2018-02-06 10:12:24 -0600

    Fix: fix player freezing issue on death.

--- js/mapMaker.js
+++ js/mapMaker.js
@@ -7,6 +7,8 @@
         let id = 0;
         for(let x = 0; x < w; x++){
             for(let y = 0; y < h; y++){
+
+                // Set default to unbroken tile
                 let obj = {
                     "id": id,
                     "pos": {
@@ -20,15 +22,20 @@
                     
                 };
 
-                if(x === 0 || x === w-1 || y === 0 || y === h-1){ // Set grid around map.
+                obj.hard.points = 0;
+
+                // Set map bounds tiles
+                if(x === 0 || x === w-1 || y === 0 || y === h-1){ 
                     obj.hard.points = -2;
                 }
 
+                // Set dwarf base tiles
                 if(x >=  1 && x <= 3 && y >= h/3 && y <= (h/3)+2){
                     obj.hard.points = 0;
                     obj.teamBase = 0;
                 }
     
+                // Set squirrel base tiles
                 if(x >=  w-5 && x <= w-2 && y >= h/3 && y <= (h/3)+2){
                     obj.hard.points = 0;
                     obj.teamBase = 1;
